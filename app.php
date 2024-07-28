cf678947ef4495ac86811f5ed149b5fd81014bad
m0r0z0vd <morozov.symfony@gmail.com>
2024-07-28 17:53:21 +0300

    Dotenv installed

--- app.php
+++ app.php
@@ -1,5 +1,10 @@
 <?php
 
+use Symfony\Component\Dotenv\Dotenv;
+
 require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/.env');
 
 $transactionsFilePath = __DIR__ . '/input.txt';
