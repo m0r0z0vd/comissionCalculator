d6995690c01adbb15b35c83fe6534d4641ee4fdc
m0r0z0vd <morozov.symfony@gmail.com>
2024-07-28 21:05:17 +0300

    Commission calculator created

---
+++ src/Services/CommissionCalculator.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace CommissionApp\Services;
+
+use CommissionApp\Exceptions\InvalidCountryDataException;
+use CommissionApp\Exceptions\InvalidUrlException;
+use CommissionApp\Structures\TransactionData;
+use Exception;
+use Money\Formatter\DecimalMoneyFormatter;
+use Money\Money;
+use Money\Currency;
+use Money\Currencies\ISOCurrencies;
+
+class CommissionCalculator
+{
+    /** @var CardNumberCountryDataRetriever */
+    private CardNumberCountryDataRetriever $countryDataRetriever;
+
+    /** @var array<string, float> */
+    private array $exchangeRates;
+
+    /** @var ISOCurrencies */
+    private ISOCurrencies $currencies;
+
+    /**
+     * @param CardNumberCountryDataRetriever $countryDataRetriever
+     * @param array<string, float> $exchangeRates
+     */
+    public function __construct(
+        CardNumberCountryDataRetriever $countryDataRetriever,
+        array $exchangeRates
+    ) {
+        $this->countryDataRetriever = $countryDataRetriever;
+        $this->exchangeRates = $exchangeRates;
+        $this->currencies = new ISOCurrencies();
+    }
+
+    /**
+     * @param TransactionData $transaction
+     * @return string
+     * @throws InvalidCountryDataException
+     * @throws InvalidUrlException
+     */
+    public function calculate(TransactionData $transaction): string
+    {
+        $country = $this->countryDataRetriever->getCountryData($transaction->bin);
+        $amount = $this->parseAmount($transaction->amount, $transaction->currency);
+        $convertedAmount = $this->convertToEur($amount);
+        $commission = $this->calculateCommission($convertedAmount, $country->isEU);
+
+        return $this->formatAmount($commission);
+    }
+
+    /**
+     * @param string $amount
+     * @param string $currency
+     * @return Money
+     */
+    private function parseAmount(string $amount, string $currency): Money
+    {
+        return new Money($amount * 100, new Currency($currency));
+    }
+
+    /**
+     * @param Money $amount
+     * @return Money
+     * @throws Exception
+     */
+    private function convertToEur(Money $amount): Money
+    {
+        $currencyCode = $amount->getCurrency()->getCode();
+
+        if ($currencyCode === 'EUR') {
+            return $amount;
+        }
+
+        if (!isset($this->exchangeRates[$currencyCode])) {
+            throw new Exception("Exchange rate for currency $currencyCode not found.");
+        }
+
+        $exchangeRate = $this->exchangeRates[$currencyCode];
+        $amountInEur = $amount->multiply($exchangeRate);
+        return new Money($amountInEur->getAmount(), new Currency('EUR'));
+    }
+
+
+    /**
+     * @param Money $amount
+     * @param bool $isEu
+     * @return Money
+     */
+    private function calculateCommission(Money $amount, bool $isEu): Money
+    {
+        $commissionRate = $isEu ? 0.01 : 0.02;
+        $commissionAmount = $amount->multiply($commissionRate);
+        return new Money($commissionAmount->getAmount(), new Currency('EUR'));
+    }
+
+    /**
+     * @param Money $amount
+     * @return string
+     */
+    private function formatAmount(Money $amount): string
+    {
+        $formatter = new DecimalMoneyFormatter($this->currencies);
+        return $formatter->format($amount);
+    }
+}
